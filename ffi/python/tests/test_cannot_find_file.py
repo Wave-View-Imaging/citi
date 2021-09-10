04c51377f4c0fd0e20077f0ba81b5fb220935251
Bryce A Besler <bryce@waveviewimaging.com>
2021-09-10 17:21:52 -0600

    Add error codes to Python FFI

--- ffi/python/tests/test_cannot_find_file.py
+++ ffi/python/tests/test_cannot_find_file.py
@@ -5,5 +5,7 @@ from citi import Record
 class TestCannotFindFile(unittest.TestCase):
 
     def test_cannot_find_file(self):
-        with self.assertRaises(NotImplementedError):
+        with self.assertRaises(NotImplementedError) as e:
             Record("filename that does not exist")
+
+        self.assertEqual(str(e.exception), 'File not found for reading')
